81e015076abe51e869f0511908b79d690e911cd9
Nguyễn Đức Anh <ducanhvna@outlook.com>
2018-08-01 22:26:09 +0700

    Update data

--- src/KaraEducationManagermentSystem/KaraMongoModel/EduSubject.cs
+++ src/KaraEducationManagermentSystem/KaraMongoModel/EduSubject.cs
@@ -1,6 +1,19 @@
-﻿namespace KaraMongoModelNS
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace KaraMongoModelNS
 {
     public class EduSubject
     {
+        [BsonId]
+        public ObjectId _id { get; set; }
+
+        public string Title { get; set; }
+        public string ShortName { get; set; }
+        public string Color { get; set; }
+        public string Picture { get; set; }
+        public short schoolId { get; set; }
+        [BsonIgnore]
+        public School school { get; set; }
     }
 }
